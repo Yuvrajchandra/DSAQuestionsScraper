c1d807e85970605b7622c13c572ed8ffc68164b3
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-12-29 17:55:23 +0530

    Removed unused login authentication codes

--- main.py
+++ main.py
@@ -22,29 +22,14 @@ def scrape():
     username.send_keys("Your Email Id")
     password.send_keys("Your Password")
     driver.find_element_by_xpath("//button[@type='submit']").click()
-    # login_url = 'https://www.pepcoding.com/login'
-    # data = {
-    #     'username': 'Singhyuvraj179@gmail.com',
-    #     'password': 'Qwerty@123'
-    # }
-    #
     time.sleep(5)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     myuls = soup.find_all('a')
     myuls = soup.findAll('span', attrs={'class': 'questions-name'})
     for ele in myuls:
         print(ele)
-    #
-    # with requests.Session() as s:
-    #     response = requests.post(login_url , data)
-    #     # print(response.text)
-    #     index_page= s.get('https://www.pepcoding.com/most-important-interview-questions-list-for-product-based-companies')
-    #     soup = BeautifulSoup(index_page.text, 'html.parser')
-    #     print(soup.title)
-    # PEPCODING_QUESTIONS_LIST_URL = 'https://www.pepcoding.com/most-important-interview-questions-list-for-product-based-companies'
-    # req = requests.get(PEPCODING_QUESTIONS_LIST_URL)
-    # soup = BeautifulSoup(req.content, 'html.parser')
-    #
+
+
     # # myuls = soup.findAll('span', attrs={'class': 'questions-name'})
     # # myuls = soup.findAll('span')
     # # myuls = soup.find_all('a', href=re.compile("^http"))
