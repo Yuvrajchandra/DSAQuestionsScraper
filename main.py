221bc0dbfd63e403288c4aa9952dfb5fcede9292
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-12-30 12:50:07 +0530

    Changed appropriate variable names

--- main.py
+++ main.py
@@ -29,40 +29,40 @@ def scrape():
     password.send_keys("Qwerty@123")
     driver.find_element_by_xpath("//button[@type='submit']").click()
     time.sleep(5)
-    Question_urls = []
+
     id = 1
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
 
-    myuls = soup.findAll('li', attrs={'class': 'collection-item'})
-    for ele in myuls:
-        children = ele.findChildren("a" , recursive=False)
+    collections = soup.findAll('li', attrs={'class': 'collection-item'})
+    for collection in collections:
+        a_tags = collection.findChildren("a" , recursive=False)
         question = {}
 
+
+        # Question Id
         question['id'] = id
 
 
         # Question Names
-        for child in children:
-            qwe=child.findChildren("span", attrs={'class': 'questions-name'}, recursive=False)
-            rty=qwe[0]
-            question['name'] = rty.text.strip()
-            # print(rty.text.strip())
+        for a_tag in a_tags:
+            span_tag = a_tag.findChildren("span", attrs={'class': 'questions-name'}, recursive=False)
+            question_name = span_tag[0]
+            question['name'] = question_name.text.strip()
 
-            
+
+
         # Question Urls
-        for child in children:
-            question['link'] = child['href']
-            # Question_urls.append(child['href'])
-            # print(child['href'])
+        for a_tag in a_tags:
+            question['link'] = a_tag['href']
+
 
 
 
         questions_data.append(question)
         id = id + 1
 
-    # print(len(myuls))
-    # print(len(Question_urls))
+
     print(questions_data)
     print(len(questions_data))
 
