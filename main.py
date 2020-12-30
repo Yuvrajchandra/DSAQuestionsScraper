ba90cef96142ef633b0af88be1e1104994f0dcd3
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-12-30 14:06:37 +0530

    Added Topics for each questions

--- main.py
+++ main.py
@@ -44,6 +44,47 @@ def scrape():
         question['id'] = id
 
 
+        # Question Topic
+        if id<=49:
+            question['topic'] = "Array"
+        elif id>49 and id<=79:
+            question['topic'] = "Searching & Sorting"
+        elif id>79 and id<=133:
+            question['topic'] = "String"
+        elif id>133 and id<=152:
+            question['topic'] = "Matrix"
+        elif id>152 and id<=171:
+            question['topic'] = "BackTracking"
+        elif id>171 and id<=193:
+            question['topic'] = "Greedy"
+        elif id>193 and id<=247:
+            question['topic'] = "Dynamic Programming"
+        elif id>247 and id<=268:
+            question['topic'] = "Stacks & Queues"
+        elif id>268 and id<=299:
+            question['topic'] = "LinkedList"
+        elif id>299 and id<=311:
+            question['topic'] = "Generic Trees"
+        elif id>311 and id<=363:
+            question['topic'] = "Binary Trees"
+        elif id>363 and id<=391:
+            question['topic'] = "Binary Search Trees"
+        elif id>391 and id<=421:
+            question['topic'] = "Order Statistics & Heap & Hash"
+        elif id>421 and id<=457:
+            question['topic'] = "Graphs"
+        elif id>457 and id<=477:
+            question['topic'] = "Bit Manipulation"
+        elif id>477 and id<=487:
+            question['topic'] = "Maths"
+        elif id>487 and id<=499:
+            question['topic'] = "Geometry"
+        elif id>499 and id<=506:
+            question['topic'] = "Trie"
+        elif id>506 and id<=508:
+            question['topic'] = "Union Find"
+
+
         # Question Names
         for a_tag in a_tags:
             span_tag = a_tag.findChildren("span", attrs={'class': 'questions-name'}, recursive=False)
