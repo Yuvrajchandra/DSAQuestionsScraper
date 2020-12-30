3e1f7a4eb27efba60351a081fd09134e0930d151
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-12-30 11:51:27 +0530

    Extracted question URLS

--- main.py
+++ main.py
@@ -13,45 +13,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 def scrape():
+
+
     PEPCODING_QUESTIONS_LIST_URL = 'https://www.pepcoding.com/most-important-interview-questions-list-for-product-based-companies'
+
+    # Setting up Chrome Driver Using Selenium
     driver = webdriver.Chrome("C:/Users/chand/Downloads/chromedriver_win32/chromedriver.exe")
     driver.get(PEPCODING_QUESTIONS_LIST_URL)
     time.sleep(3)
+
+    # Loggin into Pepcoding portal
     username = driver.find_element_by_xpath("//input[@type='email']")
     password = driver.find_element_by_xpath("//input[@type='password']")
     username.send_keys("Singhyuvraj179@gmail.com")
     password.send_keys("Qwerty@123")
     driver.find_element_by_xpath("//button[@type='submit']").click()
     time.sleep(5)
+    Question_urls = []
+
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    links_with_text = []
-    # myuls = soup.find_all('a')
-    # myuls = soup.findAll('span', attrs={'class': 'questions-name'})
-    # for ele in myuls:
-    #     print(ele)
 
     myuls = soup.findAll('li', attrs={'class': 'collection-item'})
     for ele in myuls:
         children = ele.findChildren("a" , recursive=False)
-        for child in children:
-            # print(child.findChild("span", recursive=False).text.strip())
-            qwe=child.findChildren("span", attrs={'class': 'questions-name'}, recursive=False)
-            rty=qwe[0]
-            print(rty.text.strip())
-            # print(child)
-    # for ele in myuls:
-    #     print(ele)
-    print(len(myuls))
-    # print(links_with_text)
 
-    # # myuls = soup.findAll('span', attrs={'class': 'questions-name'})
-    # # myuls = soup.findAll('span')
-    # # myuls = soup.find_all('a', href=re.compile("^http"))
-    # myuls = soup.find_all('a')
-    # print(myuls)
+
+        # Question Urls
+        for child in children:
+            Question_urls.append(child['href'])
+            # print(child['href'])
+
+        # Question Names
+        # for child in children:
+        #     qwe=child.findChildren("span", attrs={'class': 'questions-name'}, recursive=False)
+        #     rty=qwe[0]
+        #     print(rty.text.strip())
+
     # print(len(myuls))
-    # # for ele in myuls:
-    # #     print(ele)
+    print(len(Question_urls))
+
 
 
 
